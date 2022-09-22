cee9839efdcf21228ec4149271702d2512eeb497
Igor <99yearago99@gmail.com>
2022-09-22 18:44:21 +0300

    Homework 3, task 3

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+    int lp = 23;
+    int ac = 27;
+    int ea = 30;
+    int papers = 480;
+    int totalStudents = lp + ac + ea;
+    int papersForEachStudent = papers / totalStudents;
+        System.out.println("На каждого ученика рассчитано " + papersForEachStudent + " листов бумаги");
+
+    }
+}
